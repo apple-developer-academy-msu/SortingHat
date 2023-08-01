a450043ab2aec69a8b1ff57d0f16ff52e88d210b
Nick Melekian <nick.melekian@gmail.com>
2023-08-01 14:12:09 -0400

    vm sort learners func

--- SortingHat/ContentView.swift
+++ SortingHat/ContentView.swift
@@ -7,7 +7,14 @@
 
 import SwiftUI
 
+
+// As a Mentor I would like a button that sorts a list of learners into teams of five so that I can easily randomize teams for challenges
+
+// As a mentor I would like to fetch learners names from AirTable so that I can easily plug in an existing database of learners
+
+// How can we apply best practices and the new concepts we have been talking about? (Optionals, Generics, Protocolos, Extensions, etc.)
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
