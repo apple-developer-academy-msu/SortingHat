c0b01ed9bc7e54b09de6c17c6c8ff941d8dc8031
Nick Melekian <nick.melekian@gmail.com>
2023-08-01 13:22:28 -0400

    Initial Commit

---
+++ SortingHat/SortingHatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingHatApp.swift
+//  SortingHat
+//
+//  Created by Nicholas Melekian on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SortingHatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
