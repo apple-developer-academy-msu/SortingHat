d82b51f6e7e8dec9cdd3145e93c40f5931dbf7bb
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-08-02 11:18:39 -0400

    added load learners function in student randomizer

--- SortingHat/StudentRandomizer.swift
+++ SortingHat/StudentRandomizer.swift
@@ -10,6 +10,8 @@ import Foundation
 class StudentRandomizer: ObservableObject {
     @Published var learners: [Learner] = []
     @Published var sortedTeams: [Team]?
+    @Published var error: Error?
+    
     static let defaultTeamSize = 5
     
     init(learners: [Learner]) {
@@ -18,5 +20,15 @@ class StudentRandomizer: ObservableObject {
     
     func sortLearners() {
         sortedTeams = learners.shuffled().chunked(into: Self.defaultTeamSize).map { Team(members: $0) }
+    }
+    
+    func loadLearners() async {
+        let airTableService = AirtableService()
+        
+        do {
+            learners = try await airTableService.fetchLearners()
+        } catch {
+            self.error = error
+        }
     }
 }
